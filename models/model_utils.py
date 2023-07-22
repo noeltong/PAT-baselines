c5274b1539befa71035d08fe1642b2fb79bd02d7
noeltong <2441624985@qq.com>
2023-07-22 21:07:26 +0800

    change params

--- models/model_utils.py
+++ models/model_utils.py
@@ -4,17 +4,16 @@ from models.uformer import Uformer
 def get_arch(opt):
     arch = opt.arch
     if arch == 'UNet':
-        model_restoration = UNet(dim=opt.embed_dim)
+        model = UNet(dim=opt.embed_dim)
     elif arch == 'Uformer':
-        model_restoration = Uformer(img_size=opt.train_ps,embed_dim=opt.embed_dim,win_size=8,token_projection='linear',token_mlp='leff',modulator=True)
+        model = Uformer(img_size=14, embed_dim=196, win_size=8, token_projection='linear', token_mlp='leff', modulator=True)
     elif arch == 'Uformer_T':
-        model_restoration = Uformer(img_size=opt.train_ps,embed_dim=16,win_size=8,token_projection='linear',token_mlp='leff',modulator=True)
+        model = Uformer(img_size=14, embed_dim=16, win_size=8, token_projection='linear', token_mlp='leff', modulator=True)
     elif arch == 'Uformer_S':
-        model_restoration = Uformer(img_size=opt.train_ps,embed_dim=32,win_size=8,token_projection='linear',token_mlp='leff',modulator=True)
+        model = Uformer(img_size=14, embed_dim=32, win_size=8, token_projection='linear', token_mlp='leff', modulator=True)
     elif arch == 'Uformer_B':
-        model_restoration = Uformer(img_size=opt.train_ps,embed_dim=32,win_size=8,token_projection='linear',token_mlp='leff',
-            depths=[1, 2, 8, 8, 2, 8, 8, 2, 1],modulator=True,dd_in=opt.dd_in)
+        model = Uformer(img_size=14, embed_dim=32, win_size=8, token_projection='linear', token_mlp='leff', depths=[1, 2, 8, 8, 2, 8, 8, 2, 1], modulator=True, dd_in=1)
     else:
         raise Exception("Arch error!")
 
-    return model_restoration
+    return model
