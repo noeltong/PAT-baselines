e36f00f85feae9c589b25a50bca735dfc0050108
noeltong <2441624985@qq.com>
2023-07-26 22:17:24 +0800

    update params

--- models/model_utils.py
+++ models/model_utils.py
@@ -6,7 +6,7 @@ def get_arch(arch):
     if arch == 'UNet':
         model = UNet(dim=64)
     elif arch == 'Uformer':
-        model = Uformer(img_size=128, embed_dim=32, win_size=8, token_projection='linear', token_mlp='leff', modulator=True)
+        model = Uformer(img_size=128, embed_dim=64, win_size=8, token_projection='linear', token_mlp='leff', modulator=True)
     elif arch == 'Uformer_T':
         model = Uformer(img_size=128, embed_dim=16, win_size=8, token_projection='linear', token_mlp='leff', modulator=True)
     elif arch == 'Uformer_S':
