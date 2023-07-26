92dc7de7f750b5e35d6e81e2cc50f04bf0759ee6
noeltong <2441624985@qq.com>
2023-07-26 20:11:51 +0800

    update train function

--- run_lib.py
+++ run_lib.py
@@ -3,19 +3,22 @@ import os
 import torch
 from torch import nn
 from torch.nn import functional as F
-from utils.data import MyDataset
+from utils.data import load_data, get_mask_fn
 from utils.time import time_calculator
 from torch.nn.parallel import DistributedDataParallel
 import torch.distributed as dist
 from utils.optim import get_optim
 from utils.utils import seed_everything
 from torch.utils.tensorboard import SummaryWriter
-from models.ema import ExponentialMovingAverage
 from utils.utils import AverageMeter
 from models.model_utils import get_arch
 from models.loss import get_loss_fn
+from utils.PAT import DASSparseOperator
+from utils.utils import min_max_scaler
+from utils.eval import get_metric_fn
 
-def train(config, workdir, train_dir='train'):
+
+def train(args, train_dir='train'):
     """Runs the training pipeline.
 
     Args:
@@ -23,8 +26,8 @@ def train(config, workdir, train_dir='train'):
     workdir: directory to store files.
     """
 
-    assert config.distributed, "Distributed train is needed!"
     torch.backends.cudnn.benchmark = True
+    workdir = os.path.join(args.work_dir, args.mask, str(args.num_known))
     workdir = os.path.join(workdir, train_dir)
 
     # -------------------
@@ -40,9 +43,227 @@ def train(config, workdir, train_dir='train'):
     # seeds
     # -------------------
 
-    seed_everything(config.seed + rank)
+    seed_everything(args.seed + rank)
 
-    if config.use_deterministic_algorithms:
+    os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
+    torch.backends.cudnn.deterministic = True
+    torch.use_deterministic_algorithms(True)
+
+    # -----------------------------
+    # Create directories for data
+    # -----------------------------
+
+    log_dir = os.path.join(workdir, 'tensorboard')
+    ckpt_dir = os.path.join(workdir, 'ckpt')
+
+    os.makedirs(log_dir, exist_ok=True)
+    os.makedirs(ckpt_dir, exist_ok=True)
+
+    # -------------------
+    # Loggers
+    # -------------------
+
+    logger = logging.getLogger('train')
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        '%(asctime)s %(filename)s %(funcName)s [line:%(lineno)d] %(levelname)s: %(message)s')
+
+    fh = logging.FileHandler(os.path.join(
+        workdir, 'train_log.log'), encoding='utf-8')
+    sh = logging.StreamHandler()
+    fh.setFormatter(formatter)
+    sh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    # -------------------
+    # Load data
+    # -------------------
+
+    if rank == 0:
+        logger.info('Loading transforms...')
+
+    DAS = DASSparseOperator(args)
+
+    if rank == 0:
+        logger.info('Transform function loaded.')
+
+    if rank == 0:
+        logger.info('Loading data...')
+
+    train_loader, train_sampler, test_loader, test_sampler = load_data(args)
+
+    if rank == 0:
+        logger.info(f'Data loaded.')
+
+    dist.barrier()
+
+    # -------------------
+    # Initialize model
+    # -------------------
+
+    if rank == 0:
+        logger.info('Begin model initialization...')
+
+    model = get_arch(args.model.arch)
+    model = model.cuda()
+    model = DistributedDataParallel(model, device_ids=[rank])
+
+    if rank == 0:
+        logger.info("Models initialized.")
+
+    dist.barrier()
+
+    # -------------------
+    # define optimization
+    # -------------------
+
+    if rank == 0:
+        logger.info('Handling optimizations...')
+
+    optimizer, scheduler = get_optim(args)
+    criterion = get_loss_fn(args)
+
+    if rank == 0:
+        logger.info('Completed.')
+
+    # -------------------
+    # training loop
+    # -------------------
+
+    scaler = torch.cuda.amp.GradScaler()
+    writer = SummaryWriter(log_dir=log_dir)
+    time_logger = time_calculator()
+    iters_per_epoch = len(train_loader)
+    mask_fn = get_mask_fn(args)
+    get_metric = get_metric_fn(args)
+
+    dist.barrier()
+    torch.cuda.empty_cache()
+
+    for epoch in range(args.num_epochs):
+        train_sampler.set_epoch(epoch)
+        test_sampler.set_epoch(epoch)
+        model.train()
+        train_loss_epoch = AverageMeter()
+
+        if rank == 0:
+            logger.info(f'Start training epoch {epoch + 1}.')
+
+        # ----------------------------
+        # run the training process
+        # ----------------------------
+
+        for i, (x, y) in enumerate(train_loader):
+            x, y = x.float().cuda(non_blocking=True), y.float().cuda(non_blocking=True)
+            x = DAS.signal_to_image(mask_fn(x))
+            with torch.cuda.amp.autocast(enabled=True):
+                out = model(x)
+                loss = criterion(out, y)
+
+            train_loss_epoch.update(loss.item(), x.shape[0])
+            if rank == 0:
+                writer.add_scalar("Train/Loss", train_loss_epoch.val,
+                                  epoch * iters_per_epoch + i)
+
+            logger.info(
+                f'Epoch: {epoch + 1}/{args.num_epochs}, Iter: {i + 1}/{iters_per_epoch}, Loss: {train_loss_epoch.val:.6f}, Device: {rank}')
+
+            optimizer.zero_grad()
+            scaler.scale(loss).backward()
+
+            scaler.step(optimizer)
+            scaler.update()
+
+        scheduler.step()
+
+        dist.barrier()
+        if rank == 0:
+            logger.info(
+                f'Epoch: {epoch + 1}/{args.num_epochs}, Avg loss: {train_loss_epoch.avg:.4f}, Time: {time_logger.time_length()}')
+
+        # save snapshot periodically
+        if (epoch + 1) % args.snap_freq == 0:
+            states = model.state_dict()
+            if rank == 0:
+                logger.info(f'Saving snapshot at epoch {epoch + 1}')
+                torch.save(states, os.path.join(
+                    ckpt_dir, f'{epoch+1}_loss_{train_loss_epoch.avg:.2f}.pth'))
+
+        # Report loss on eval dataset periodically
+        if (epoch + 1) % args.eval_freq == 0:
+            if rank == 0:
+                logger.info(f'Start evaluate at epoch {epoch + 1}.')
+
+            with torch.no_grad():
+                model.eval()
+                iters_per_eval = len(test_loader)
+                eval_loss_epoch = AverageMeter()
+                eval_psnr_epoch = AverageMeter()
+                eval_ssim_epoch = AverageMeter()
+
+                for i, (x, y) in enumerate(test_loader):
+                    x, y = x.float().cuda(non_blocking=True), y.float().cuda(non_blocking=True)
+                    x = DAS.signal_to_image(mask_fn(x))
+                    with torch.cuda.amp.autocast(enabled=True):
+                        out = model(x)
+                        loss = criterion(out, y)
+
+                    for n in range(x.shape[0]):
+                        p, s = get_metric(min_max_scaler(out[n, ...].squeeze()), min_max_scaler(y[n, ...].squeeze()))
+                        eval_psnr_epoch.update(p)
+                        eval_ssim_epoch.update(s)
+                    
+
+                    eval_loss_epoch.update(loss.item(), x.shape[0])
+                    logger.info(
+                        f'Epoch: {epoch + 1}/{args.num_epochs}, Iter: {i + 1}/{iters_per_eval}, Loss: {eval_loss_epoch.val:.6f}, Time: {time_logger.time_length()}, PSNR: {eval_psnr_epoch.val}, SSIM: {eval_ssim_epoch.val}, Device: {rank}')
+
+                if rank == 0:
+                    writer.add_scalar('Eval/Eval loss', eval_loss_epoch.avg, epoch)
+                    writer.add_scalar('Eval/PSNR', eval_psnr_epoch.avg, epoch)
+                    writer.add_scalar('Eval/SSIM', eval_ssim_epoch.avg, epoch)
+
+                if rank == 0:
+                    logger.info(
+                        f'Epoch: {epoch + 1}/{args.num_epochs}, Avg eval loss: {eval_loss_epoch.avg:.4f}, PSNR: {eval_psnr_epoch.avg}, SSIM: {eval_ssim_epoch.avg}.')
+
+        dist.barrier()
+
+    states = model.state_dict()
+    if rank == 0:
+        logger.info(
+            f'Training complete. Total time: {time_logger.time_length()}.')
+        torch.save(states, os.path.join(ckpt_dir, 'final.pth'))
+
+
+def eval(args, eval_dir='eval'):
+    """Runs the evaluation pipeline.
+
+    Args:
+    config: ml_collections.ConfigDict(), config of the project
+    workdir: directory to store files.
+    """
+
+    torch.backends.cudnn.benchmark = True
+    workdir = args.work_dir
+    workdir = os.path.join(workdir, eval_dir)
+
+    # -------------------
+    # Initialize DDP
+    # -------------------
+
+    dist.init_process_group(backend='nccl')
+    rank = dist.get_rank()
+    torch.cuda.set_device(rank)
+    device = torch.device("cuda", rank)
+
+    # -------------------
+    # seeds
+    # -------------------
+
+    seed_everything(args.seed + rank)
+
+    if args.use_deterministic_algorithms:
         os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
         torch.backends.cudnn.deterministic = True
         torch.use_deterministic_algorithms(True)
@@ -76,15 +297,13 @@ def train(config, workdir, train_dir='train'):
     # -------------------
 
     if rank == 0:
-        logger.info('Loading data...')
+        logger.info('Loading transforms...')
 
-    # train_loader, test_loader, train_sampler, test_sampler = pass
-
-    dist.barrier()
+    DAS = DASSparseOperator(args)
 
     if rank == 0:
-        logger.info('Data loaded.')
-
+        logger.info('Transform function loaded.')
+    
     # -------------------
     # Initialize model
     # -------------------
@@ -92,185 +311,51 @@ def train(config, workdir, train_dir='train'):
     if rank == 0:
         logger.info('Begin model initialization...')
 
-    model = get_arch(config)
-    model = nn.SyncBatchNorm.convert_sync_batchnorm(model)
-    # If model has BNs...
-
+    model = get_arch(args.model.arch)
+    model.load_state_dict(
+        torch.load(args.ckpt, map_location='cpu')
+    )
     model = model.cuda()
-    model = DistributedDataParallel(model, device_ids=[rank])
-    model_without_ddp = model.module
-
-    if config.model.ema:
-        adjust = config.training.batch_size * \
-            config.model.ema_steps / config.training.num_epochs
-        alpha = 1 - config.model.ema_rate
-        alpha = 1 - min(1.0, alpha * adjust)
-        model_ema = ExponentialMovingAverage(
-            model_without_ddp, device=device, decay=alpha)
 
     if rank == 0:
         logger.info("Models initialized.")
 
     dist.barrier()
 
-    # -------------------
-    # define optimization
-    # -------------------
+    def get_data(args, path):
+        data = np.load(path)
+        sinogram = data['sinogram']
+        gt = data['gt']
 
-    if rank == 0:
-        logger.info('Handling optimizations...')
+        mask = get_mask_fn(args)
+        masked = mask * sinogram
+        masked = torch.from_numpy(masked)
+        noisy = from_space(masked)
 
-    optimizer, scheduler = get_optim(model, config)
-    criterion = get_loss_fn(config)
-
-    if rank == 0:
-        logger.info('Completed.')
-
-    # -------------------
-    # training loop
-    # -------------------
-
-    scaler = torch.cuda.amp.GradScaler()
-    writer = SummaryWriter(log_dir=log_dir)
-    time_logger = time_calculator()
-
-    best_loss = 999999999.
-    iters_per_epoch = len(train_loader)
-
-    dist.barrier()
-    torch.cuda.empty_cache()
-
-    for epoch in range(config.training.num_epochs):
-        train_sampler.set_epoch(epoch)
-        test_sampler.set_epoch(epoch)
-        model.train()
-        train_loss_epoch = AverageMeter()
-
-        if rank == 0:
-            logger.info(f'Start training epoch {epoch + 1}.')
-
-        # ----------------------------
-        # initialize data prefetcher
-        # ----------------------------
-
-        train_prefetcher = prefetcher(train_loader, rank)
-        x, y = train_prefetcher.next()
-        i = 0
-
-        # ----------------------------
-        # run the training process
-        # ----------------------------
-
-        while x is not None:
-            with torch.cuda.amp.autocast(enabled=True):
-                out = model(x)
-                loss = criterion(out, y)
-
-            train_loss_epoch.update(loss.item(), x.shape[0])
-
-            if rank == 0:
-                writer.add_scalar("Loss", train_loss_epoch.val,
-                                  epoch * iters_per_epoch + i)
-
-            logger.info(
-                f'Epoch: {epoch + 1}/{config.training.num_epochs}, Iter: {i + 1}/{iters_per_epoch}, Loss: {train_loss_epoch.val:.6f}, Device: {rank}')
-
-            optimizer.zero_grad()
-            scaler.scale(loss).backward()
-
-            if config.model.clip_grad_norm is not None:
-                scaler.unscale_(optimizer)
-                nn.utils.clip_grad_norm_(
-                    model.parameters(), config.model.clip_grad_norm)
-
-            scaler.step(optimizer)
-            scaler.update()
-
-            if config.model.ema and i % config.model.ema_steps == 0:
-                model_ema.update_parameters(model)
-
-            x, y = train_prefetcher.next()
-            i += 1
-
-        scheduler.step()
-
-        dist.barrier()
-        if rank == 0:
-            logger.info(
-                f'Epoch: {epoch + 1}/{config.training.num_epochs}, Avg loss: {train_loss_epoch.avg:.4f}, Time: {time_logger.time_length()}')
-
-        # save snapshot periodically
-
-        if (epoch + 1) % config.training.save_ckpt_freq == 0:
-            if rank == 0:
-                logger.info(f'Saving snapshot at epoch {epoch + 1}')
-                snapshot = {
-                    'epoch': epoch + 1,
-                    'model': model_without_ddp.state_dict(),
-                    'optimizer': optimizer.state_dict(),
-                    'scheduler': scheduler.state_dict(),
-                    'scaler': scaler.state_dict()
-                }
-                if config.model.ema:
-                    snapshot['model_ema'] = model_ema.state_dict()
-                torch.save(snapshot, os.path.join(
-                    ckpt_dir, f'{epoch+1}_loss_{train_loss_epoch.avg:.2f}.pth'))
-
-        is_best = train_loss_epoch.avg < best_loss
-        if is_best:
-            best_loss = train_loss_epoch.avg
-            if rank == 0:
-                logger.info(
-                    f'Saving best model state dict at epoch {epoch + 1}.')
-                torch.save(model_ema.state_dict() if config.model.ema else model_without_ddp.state_dict,
-                           os.path.join(ckpt_dir, 'best.pth'))
-
-        # Report loss on eval dataset periodically
-
-        if (epoch + 1) % config.training.eval_freq == 0:
-            if rank == 0:
-                logger.info(f'Start evaluate at epoch {epoch + 1}.')
-
-            eval_model = model_ema if config.model.ema else model_without_ddp
-            # eval_model = model_without_ddp
-            with torch.inference_mode():
-                eval_model.eval()
-                iters_per_eval = len(test_loader)
-                eval_loss_epoch = AverageMeter()
-
-                # ----------------------------
-                # initialize data prefetcher
-                # ----------------------------
-
-                test_prefetcher = prefetcher(test_loader, rank, mode='train')
-                x, y = test_prefetcher.next()
-                i = 0
-
-                while x is not None:
-                    with torch.cuda.amp.autocast(enabled=True):
-                        out = model(x)
-                        loss = criterion(out, y)
-
-                    eval_loss_epoch.update(loss.item(), x.shape[0])
-                    logger.info(
-                        f'Epoch: {epoch + 1}/{config.training.num_epochs}, Iter: {i + 1}/{iters_per_eval}, Loss: {eval_loss_epoch.val:.6f}, Time: {time_logger.time_length()}, Device: {rank}')
-
-                    x, y = test_prefetcher.next()
-                    i += 1
-
-                if rank == 0:
-                    writer.add_scalar('Eval loss', eval_loss_epoch.avg, epoch)
-
-            if rank == 0:
-                logger.info(
-                    f'Epoch: {epoch + 1}/{config.training.num_epochs}, Avg eval loss: {eval_loss_epoch.avg:.4f}, Time: {time_logger.time_length()}')
-
-        dist.barrier()
-
-    if rank == 0:
-        logger.info(
-            f'Training complete.\nTotal time:, {time_logger.time_length()}')
+        return noisy
 
 
-def eval(config, workdir, eval_dir='eval'):
-    pass
+    paths = glob(os.path.join(args.data.data_dir, "*.npz"))
+    for path in paths:
+        
+        with torch.inference_mode():
+            out = model
+
+def main(args):
+    if args.mode == 'train':
+        train(args)
+    elif args.mode == 'eval':
+        eval(args)
+
+
+# def launch(args):
+#     if args.mode == 'train':
+#         for args.mask in ['limited', 'uniform', 'random']:
+#             for args.num_known in [16, 32, 64]:
+#                 train(args)
+#     elif args.mode == 'eval':
+#         eval(args)
+
+if __name__ == '__main__':
+    args = get_args()
+    main(args)
